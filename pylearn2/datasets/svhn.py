sandeepkbhat/pylearn2

commit d6050106f137147ba976e472748ad77273218b55

Modified all super() parameters, and write a new test for target_format

pylearn2/datasets/svhn.py
@@ -101,7 +101,8 @@ class SVHN(dense_design_matrix.DenseDesignMatrixPyTables):
 
         view_converter = dense_design_matrix.DefaultViewConverter((32, 32, 3),
                                                                   axes)
-        super(SVHN, self).__init__(X=data.X, y=data.y,
+        super(SVHN, self).__init__(X=data.X, y=data.y, 
+                                   y_labels=numpy.max(data.y)+1, 
                                    view_converter=view_converter)
 
         if preprocessor:

pylearn2/datasets/csv_dataset.py
@@ -62,7 +62,6 @@ class CSVDataset(DenseDesignMatrix):
     end_fraction : float
       The fraction of rows, starting at the end of the file, to load.
     """
-
     def __init__(self,
                  path='train.csv',
                  task='classification',
@@ -124,12 +123,11 @@ class CSVDataset(DenseDesignMatrix):
         # and go
         self.path = preprocess(self.path)
         X, y = self._load_data()
-        
+
         if self.task == 'regression':
             super(CSVDataset, self).__init__(X=X, y=y)
         else:
-            super(CSVDataset, self).__init__(X=X, y=y, y_labels=np.max(y)+1 )
-
+            super(CSVDataset, self).__init__(X=X, y=y, y_labels=np.max(y)+1)
 
     def _load_data(self):
         """
@@ -167,6 +165,9 @@ class CSVDataset(DenseDesignMatrix):
         if self.expect_labels:
             y = data[:, 0]
             X = data[:, 1:]
+            
+            if self.task == 'regression':
+                y = y.reshape((y.shape[0], 1))
         else:
             X = data
             y = None

pylearn2/datasets/icml07.py
@@ -63,7 +63,7 @@ class ICML07DataSet(DenseDesignMatrix):
         data_y = data_y[start:end]
 
         view_converter = DefaultViewConverter((28, 28, 1))
-        super(ICML07DataSet, self).__init__(X=data_x, y=data_y, 
+        super(ICML07DataSet, self).__init__(X=data_x, y=data_y,
             y_labels=data_y.max() + 1, view_converter=view_converter)
 
     def get_test_set(self):

pylearn2/datasets/mnistplus.py
@@ -87,6 +87,6 @@ class MNISTPlus(dense_design_matrix.DenseDesignMatrix):
 
         # init the super class
         super(MNISTPlus, self).__init__(
-            X=data_x, y=data_y, view_converter=view_converter)
+            X=data_x, y=data_y, y_labels=np.max(data_y)+1, view_converter=view_converter)
 
         assert not contains_nan(self.X)

pylearn2/datasets/new_norb.py
@@ -430,6 +430,7 @@ class NORB(DenseDesignMatrix):
 
         super(NORB, self).__init__(X=images,
                                    y=labels,
+                                   y_labels=numpy.max(labels)+1,
                                    view_converter=view_converter)
 
         # Needed for pickling / unpickling.

pylearn2/datasets/norb.py
@@ -187,6 +187,7 @@ class SmallNORB(dense_design_matrix.DenseDesignMatrix):
 
         super(SmallNORB, self).__init__(X=X,
                                         y=y,
+                                        y_labels=numpy.max(y)+1,
                                         view_converter=view_converter)
 
     @classmethod

pylearn2/datasets/norb_small.py
@@ -71,7 +71,7 @@ class NORBSmall(dense_design_matrix.DenseDesignMatrix):
 
         view_converter = dense_design_matrix.DefaultViewConverter((96, 96, 2))
 
-        super(NORBSmall, self).__init__(X=X, y=y, y_labels=np.max(y),
+        super(NORBSmall, self).__init__(X=X, y=y, y_labels=np.max(y)+1,
                                         view_converter=view_converter)
 
 
@@ -144,7 +144,7 @@ class FoveatedNORB(dense_design_matrix.DenseDesignMatrix):
         view_converter = retina.RetinaCodingViewConverter((96, 96, 2),
                                                           (8, 4, 2, 2))
 
-        super(FoveatedNORB, self).__init__(X=X, y=y, y_labels=np.max(y),
+        super(FoveatedNORB, self).__init__(X=X, y=y, y_labels=np.max(y)+1,
                                            view_converter=view_converter,
                                            preprocessor=preprocessor)
 

pylearn2/datasets/ocr.py
@@ -74,7 +74,7 @@ class OCR(dense_design_matrix.DenseDesignMatrix):
 
         view_converter = dense_design_matrix.DefaultViewConverter(
             (16, 8, 1), axes)
-        super(OCR, self).__init__(X=data_x, y=data_y, y_labels=len(letters), 
+        super(OCR, self).__init__(X=data_x, y=data_y, y_labels=len(letters),
                                   view_converter=view_converter)
 
         assert not contains_nan(self.X)

commit d4f0a4cc2ae3ab225be16b7b9091da272b605f38

Fixed issues in SVHN class
[fix] super(SVHN, self) init method has no keyword argument y_labels.
      Removed this. This was wrongly introduced in the commit
      d6050106f137147ba976e472748ad77273218b55
[fix] split_train_valid method is expected to return two tuples, but it
      was returning 4 values. Grouped the 4 values as two tuples, each with
      two values. This was wrongly introduced during removal of one-hot
      in commit af07b6cc076942ef4f80c84ec7c7148afb3999ad
[chg] Added make_svhn_pytables.py to convert the downloaded SVHN format 2 .mat files
      to pytables.

pylearn2/datasets/svhn.py
@@ -103,7 +103,6 @@ class SVHN(dense_design_matrix.DenseDesignMatrixPyTables):
         view_converter = dense_design_matrix.DefaultViewConverter((32, 32, 3),
                                                                   axes)
         super(SVHN, self).__init__(X=data.X, y=data.y,
-                                   y_labels=numpy.max(data.y) + 1,
                                    view_converter=view_converter)
 
         if preprocessor:
@@ -142,7 +141,7 @@ class SVHN(dense_design_matrix.DenseDesignMatrixPyTables):
         rng = make_np_rng(None, 322, which_method="shuffle")
 
         def design_matrix_view(data_x):
-            """reshape data_x to deisng matrix view
+            """reshape data_x to design matrix view
             """
             data_x = numpy.transpose(data_x, axes=[3, 2, 0, 1])
             data_x = data_x.reshape((data_x.shape[0], 32 * 32 * 3))
@@ -235,8 +234,8 @@ class SVHN(dense_design_matrix.DenseDesignMatrixPyTables):
             train_x = numpy.cast[config.floatX](train_x)
             valid_x = numpy.cast[config.floatX](valid_x)
 
-            return design_matrix_view(train_x), train_y,\
-                design_matrix_view(valid_x), valid_y
+            return (design_matrix_view(train_x), train_y),\
+                (design_matrix_view(valid_x), valid_y)
 
         # The original splits
         if which_set in ['train', 'test']:

pylearn2/scripts/datasets/make_svhn_pytables.py
@@ -1,0 +1,40 @@
+import os
+import logging
+import shutil
+from theano import config
+from pylearn2.datasets import preprocessing
+from pylearn2.datasets.svhn import SVHN
+from pylearn2.utils.string_utils import preprocess
+
+orig_path  = preprocess('${PYLEARN2_DATA_PATH}/SVHN/format2')
+
+# Check if MAT files have been downloaded
+if not os.path.isdir(orig_path):
+    raise IOError("You need to download the SVNH format2 dataset MAT files "
+                     "before running this conversion script.")
+
+# Create directory in which to save the pytables files
+local_path = orig_path
+if not os.path.isdir(os.path.join(local_path, 'h5')):
+    os.makedirs(os.path.join(local_path, 'h5'))
+
+print """
+      ***************************************************************
+      Please ignore the warning produced during this MAT -> Pytables
+      conversion for the SVNH dataset. Inspite of what it says, the
+      files are not modified.
+      ***************************************************************
+      """
+
+test = SVHN('test', path=local_path+'/')
+check_dtype(test)
+
+valid = SVHN('valid', path=local_path)
+check_dtype(valid)
+
+train = SVHN('splitted_train', path=local_path)
+check_dtype(train)
+
+
+
+